465684baaf0fad6c80d0e9803a69f46a7797de00
wynterwolf <shinyguns@gmail.com>
2020-11-05 13:51:12 -0600

    added associations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_many :appointments
+  has_many :doctors, through: :appointments
 
   validates :name, presence: true, uniqueness: {scope: :user_id}
 end
