f8c7e12a49cafe05107dc67a157cdf8b6a95a03f
wynterwolf <shinyguns@gmail.com>
2020-11-05 14:37:00 -0600

    update index add show views

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,15 @@
 class PetsController < ApplicationController
 
+    #helper from devise
+    before_action :authenticate_user! 
+
     def index
+        @pets = current_user.pets
     end
 
-    
+    def show
+        @pet = current_user.pets.find(
+        params[:id]
+        )
+    end
 end
