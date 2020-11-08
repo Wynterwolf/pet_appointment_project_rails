789fd115627273cd8e730dde8ed6bcd348ba140d
wynterwolf <shinyguns@gmail.com>
2020-11-08 15:51:28 -0600

    added vet views & controller actions

--- app/controllers/veterinarians_controller.rb
+++ app/controllers/veterinarians_controller.rb
@@ -1,2 +1,38 @@
 class VeterinariansController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_veterinarian, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @veterinarians = Veterinarian.all
+    end
+
+    def show
+        # @ = .find()
+    end
+
+    def new
+        @veterinarian = Veterinarian.new
+    end
+
+    def create
+         @veterinarian = Veterinarian.new(vet_params)
+            if @veterinarian.save 
+                redirect_to veterinarian_path(@veterinarian)
+            else
+                render :new
+            end
+    end
+
+    private
+
+    def set_vet
+        @veterinarian = Veterinarian.find(
+        params[:id]
+        )
+    end
+
+    def vet_params
+        params.require(:veterinarian).permit(:name, :phone_number, :specializations)
+    end
+
 end
