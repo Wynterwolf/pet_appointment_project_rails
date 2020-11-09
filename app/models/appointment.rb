c6cccacb344580a41c543d79be316b80d20f1f4d
wynterwolf <shinyguns@gmail.com>
2020-11-08 21:01:44 -0600

    model & view & partial form for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,21 +3,21 @@ class Appointment < ApplicationRecord
   belongs_to :pet
 
   validates :start_time, :end_time, :location, presence: true
-  validate :vet_overlap
-  validate :pet_overlap
-  validate :ends_after_start
+  validate :vet_overlap, :pet_overlap, if: :starts_before_it_ends?
+  validate :ends_after_it_starts
   
   #Don't forget about doublebooking the appointments
   def vet_overlap
     this_start = self.start_time
     this_end = self.end_time
+    return if this_end < this_start
     overlap = veterinarian.appointments.any? do |appointment|
       other_start = appointment.start_time
       other_end = appointment.end_time
       other_start < this_end && this_end < other_end || other_start < this_start && this_start < other_end
     end
     if overlap
-      errors.add(:doctor, 'has a conflicting appointment')
+      errors.add(:veterinarian, 'has a conflicting appointment')
     end
   end
 #What if the pet is doublebooked
@@ -33,10 +33,23 @@ class Appointment < ApplicationRecord
       errors.add(:pet, 'has a conflicting appointment')
     end
   end
-#keeps appointments start time before end time
-  def ends_after_start
-    if start_time > end_time
-      errors.add(:start_time, 'must start before the appointment end')
+#keeps appointments straight (ends after start, start after ends)
+  def ends_after_it_starts
+    if !starts_before_it_ends?
+      errors.add(:start_time, 'must be before the end time')
     end
+  end
+
+  def starts_before_it_ends?
+    start_time < end_time
+  end
+
+  #define doctor to use on appointment index view
+  def vet_name
+    self.veterinarian.name
+  end
+
+  def pet_name
+    self.pet.name
   end
 end
