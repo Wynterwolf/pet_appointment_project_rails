d28180b697511b18d3cba69d4a0c1ad608bd6d88
wynterwolf <shinyguns@gmail.com>
2020-11-10 14:04:55 -0600

    nested routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :appointments
   resources :veterinarians
-  resources :pets
+  resources :pets do 
+    #nested routes
+    resources :veterinarians, only: [:index]
+    resources :appointments, only: [:index, :new]
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
