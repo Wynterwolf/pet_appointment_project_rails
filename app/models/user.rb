e191eeb4c63cefb95c49ef631b1eba6fc1361c40
wynterwolf <shinyguns@gmail.com>
2020-11-11 13:28:05 -0600

    setup oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+#for omniauth
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :veterinarians, only: [:index]
     resources :appointments, only: [:index, :new]
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pets#index'
