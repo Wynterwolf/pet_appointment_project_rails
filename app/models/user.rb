dc6144c8d7bc951e3dfdec0257cc93aab2155565
wynterwolf <shinyguns@gmail.com>
2020-11-11 14:35:43 -0600

    omniauth part2, callback controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :pets
   has_many :veterinarians, through: :pets
   has_many :appointments, through: :pets
@@ -10,4 +11,15 @@ class User < ApplicationRecord
 #for omniauth
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
+  #validates oauth
+    def self.from_google(uid:, email:, full_name:, avatar_url:)
+    user = User.find_or_create_by(email: email) do |u|
+      u.uid = uid
+      u.full_name = full_name
+      u.avatar_url = avatar_url
+      u.password = SecureRandom.hex
+    end
+    user.update(uid: uid, full_name: full_name, avatar_url: avatar_url)
+    byebug
+  end
 end
